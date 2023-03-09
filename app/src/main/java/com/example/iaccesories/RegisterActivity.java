4a4429ec7ef48c49f2438304a29597fce0d5cf89
e.aparicio <eapariciobarroso@gmail.com>
2023-03-09 18:00:46 +0100

    commit5: Registro echo y conectado con la base de datos, falta solucionar error de los metodos. OnCompleteListener

--- app/src/main/java/com/example/iaccesories/RegisterActivity.java
+++ app/src/main/java/com/example/iaccesories/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,13 +43,15 @@ public class RegisterActivity extends AppCompatActivity {
                 String password = mETpassword.getText().toString();
 
 
-                /*mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
+               /*mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                   @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
-                        if( task.isSuccessful()) {
-                            mUsuariActual = mAuth.getCurrentUser();
-                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
                     }
                 });*/
             }
