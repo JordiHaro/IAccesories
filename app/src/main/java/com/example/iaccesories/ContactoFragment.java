51ef5c4594cc3bf9856cb07e6994259d48a3c715
e.aparicio <eapariciobarroso@gmail.com>
2023-04-12 20:18:32 +0200

    commit9: Prueba de menu solo sale el inicio en el pie de la pantalla, preguntar al Arnau

---
+++ app/src/main/java/com/example/iaccesories/ContactoFragment.java
@@ -0,0 +1,18 @@
+package com.example.iaccesories;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contacto, container, false);
+    }
+}
