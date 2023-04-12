caec4bc382ce3f7a1d18de78dfa4d842d66480ee
e.aparicio <eapariciobarroso@gmail.com>
2023-04-12 20:18:51 +0200

    commit9: Prueba de menu solo sale el inicio en el pie de la pantalla, preguntar al Arnau

--- app/src/main/java/com/example/iaccesories/PantallaPrincipal.java
+++ app/src/main/java/com/example/iaccesories/PantallaPrincipal.java
@@ -3,12 +3,50 @@ package com.example.iaccesories;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class PantallaPrincipal extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    InicioFragment inicioFragment = new InicioFragment();
+    NoticiasFragment noticiasFragment = new NoticiasFragment();
+    ForoFragment foroFragment = new ForoFragment();
+    ContactoFragment contactoFragment = new ContactoFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
-    }
+
+        bottomNavigationView = findViewById(R.id.bottom_navegation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, inicioFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Inicio:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new InicioFragment()).commit();
+                        return true;
+                    case R.id.Noticias:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, new NoticiasFragment()).commit();
+                        return true;
+                    case R.id.Foro:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, foroFragment).commit();
+                        return true;
+                    case R.id.Contacto:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, contactoFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
 }
+
+        }
